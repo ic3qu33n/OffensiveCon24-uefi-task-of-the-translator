934df458f7330d2ef3441debda277e5808acc220
v1kt0r_frnknstn@proton.me <v1kt0r_frnknstn@proton.me>
2024-04-21 01:54:35 -0400

    adds updated uefi_aarch64_hello_world assembly file with code fixes for loading string address

--- uefi_aarch64_hello_world/uefi-aarch64-hello-world.s
+++ uefi_aarch64_hello_world/uefi-aarch64-hello-world.s
@@ -1,7 +1,8 @@
 //Simple test hello world UEFI app written in aarch64 assembly
 
 .text
-global _start
+.globl _start
+.globl UefiMain
 .align 4
 //x0 : Image Handle
 //x1: System Table
@@ -17,24 +18,27 @@ global _start
 
 
 _start:
+	b UefiMain
+
+UefiMain:
 	//function prologue
 	stp	x29, x30, [sp, #0x30]
 	mov	x29, sp
-	str	x0, [sp, -#0x18] //store imageHandle var on stack
-	str	x1, [sp, -#0x20] //store efiSystemTable var on stack
-	ldr x0, [sp, -#0x20] //load efiSystemTable into x0
+	str	x0, [sp, #-0x18] //store imageHandle var on stack
+	str	x1, [sp, #-0x20] //store efiSystemTable var on stack
+	ldr x0, [sp, #-0x20] //load efiSystemTable into x0
 	ldr x0, [x0, #0x60] // load x0 =SystemTable + 0x60 == gBS
-	str x0, [sp, -#0x8]	//store gBS var on stack
-	ldr x0, [sp, -#0x8] //load x0 = gBS
+	str x0, [sp, #-0x8]	//store gBS var on stack
+	ldr x0, [sp, #-0x8] //load x0 = gBS
 	ldr x0, [x0, #0x140] // gBS->LocateProtocol
-	str x0, [sp, -#0x10] //store locateProtocol var on stack
-	ldr x0, [sp, -#0x20] //load x0 = SystemTable (from stack at sp - #0x20)
-	ldr x0, [x0, #0x40], //load x0 = ConOut (SystemTable + 0x40)
+	str x0, [sp, #-0x10] //store locateProtocol var on stack
+	ldr x1, [sp, #-0x20] //load x0 = SystemTable (from stack at sp - #0x20)
+	ldr x0, [x1, #0x40] //load x0 = ConOut (SystemTable + 0x40)
 	ldr x3, [x0, #0x8] //load x3 =  OutputString = SystemTable->ConOut->OutputString== x0 + 0x8 == ConOut + 0x8 == OutputString
-	ldr x0, [sp, -#0x20] //load x0 = SystemTable (from stack at sp - #0x20)
+	ldr x0, [sp, #-0x20] //load x0 = SystemTable (from stack at sp - #0x20)
 	ldr x2, [x0, #0x40] //load x2 = ConOut == x0 + 0x40 = SystemTable + 0x40
-	adrp x1, hellostr@PAGE
-	add x1, x1, hellostr@PAGEOFF
+	adrp x1, hellostr
+	add x1, x1, hellostr
 	mov x0, x2 //mov x0 = SystemTable->ConOut == x2
 	blr x3	//call SystemTable->ConOut->OutputString(SystemTable->ConOut, L"hello from the other side\n");
 	//function epilogue
@@ -43,5 +47,6 @@ _start:
 	ret	
 
 .data
+.align 4
 hellostr:
-	.unicode u"Hello from the other side\n"
+	.asciz "Hello from the other side\n"
